a9f483194bb956fa2dc8cb2a134157059295cda3
levelhlawrence <levelhlawrence@gmail.com>
2025-08-25 18:50:20 -0400

    creating django models

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Vendor
+from .models import Vehicle
 
 # Register your models here.
+
+admin.site.register(Vendor)
+admin.site.register(Vehicle)
